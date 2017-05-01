f0c7f28f96c2eef45dc90f70c735db403a215f2c
Paul Apelt <Kempf@users.noreply.github.com>
2017-05-01 16:46:39 +1000

    Delete DetectRedBird.m

--- DetectRedBird.m
+++
@@ -1,8 +0,0 @@
-function [bird] = DetectBird(frame,LB,UB)
-    frame = bwareafilt(frame,[LB,UB]);
-    SE = strel('rectangle',[5,5]);
-    frame = imdilate(frame,SE);
-    bird = regionprops(frame,'BoundingBox','Centroid');
-
-    
-end
