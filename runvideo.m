f00ea42600a5b318805d3ffa30236cd1110b4228
Jae Sim <wodnjs9966@gmail.com>
2017-05-01 20:55:47 +1000

    Deleted unecessary codes

--- runvideo.m
+++ runvideo.m
@@ -16,28 +16,17 @@ while hasFrame(video)
     subplot(1,2,1)
     image(frame)
     
-%     [FV,HV] = extractHOGFeatures(frame,'CellSize',[1,1],'BlockSize',[1,1]);
-%     hold on
-% %     plot(HV)
+
     drawnow
-% %     imwrite(frame,[num2str(fn),'.png'])
-%     fn = fn + 1;
-%     
-    
 
-%     %%
-% %     frame(:,:,1) = medfilt2(frame(:,:,1), [10 10]);
-%     frame_red_bird = CropColour(frame,[180,220,0,20,35,65]);
-%     Bird_R = DetectBird(frame_red_bird,10,inf);
-%     rec_R = DrawRectangle(Bird_R,'r');
-% 
+    frame_red_bird = CropColour(frame,[180,220,0,20,35,65]);
 
-    frame_black_bird = CropColour(frame,[0,10,0,10,0,10]);
-    frame_black_bird= imdilate(frame_black_bird,SE);
-    frame_black_bird = bwareafilt(frame_black_bird,[1,inf]);
+
+
+
 
    subplot(1,2,2)
-   image(frame_black_bird*1000)
+   image(frame_red_bird*1000)
     
 
 
@@ -47,9 +36,8 @@ while hasFrame(video)
         toc_1 = toc;
         time_per_frame = (toc_1 - toc_0)/10;
         frame_per_sec = round(1/time_per_frame)
-%         title(['Frame Rate: ', num2str(frame_per_sec)])
         toc_0 = toc_1;
     end
     frame_count = frame_count + 1;
-%     delete(rec_R)
+
 end
