e32ddf48c0d6f1511db90ab5681c3d631adb891a
JaeSim0102 <wodnjs9966@gmail.com>
2017-05-02 14:09:09 +1000

    2/5

---
+++ Filter_Black.m
@@ -0,0 +1,23 @@
+function [frame_out,region] = Filter_Black(frame)
+    SE = strel('disk',5);
+    frame_out = imclose(frame,SE);
+    region = regionprops(frame_out,'BoundingBox');
+    
+%     if (size(region,1) ~= 0) && (size(region,1) < 4)
+        for i = 1:size(region,1)
+            area = region(i).BoundingBox(3) * region(i).BoundingBox(4);
+            if (area <= 700) && (area >= 210) &&(region(i).BoundingBox(3)<30)...
+                    &&(region(i).BoundingBox(4)<30)
+                region(i).BoundingBox(1) = region(i).BoundingBox(1) - 3;
+                region(i).BoundingBox(2) = region(i).BoundingBox(2) - 3;
+                region(i).BoundingBox(3) = region(i).BoundingBox(3) + 5;
+                region(i).BoundingBox(4) = region(i).BoundingBox(4) + 5;
+                rec = rectangle('Position',region(i).BoundingBox,'EdgeColor','k','LineWidth',4);
+            end
+        
+        end
+    
+%     end
+
+
+end
