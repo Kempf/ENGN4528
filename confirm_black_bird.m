b5bfe50f17ac779e05a57970dd50c814275d7b90
Jae Sim <wodnjs9966@gmail.com>
2017-05-03 23:02:53 +1000

    Applied PCA to blackbird

--- confirm_black_bird.m
+++ confirm_black_bird.m
@@ -1,20 +1,23 @@
-function coord = confirm_red_bird(frame,img_ave_red,eigb_red,colour_detected_rec)
-
-if isempty(colour_detected_rec) == 0  
+function coord = confirm_black_bird(frame,img_ave_black,eigb_black,colour_detected_rec)
+if isempty(colour_detected_rec) == 0
     for rec = colour_detected_rec'
         frame_w = frame(rec(2):rec(4)+rec(2),...
             rec(1):rec(3)+rec(1),:);
-        dist = CalcDist_Red(frame_w,img_ave_red,eigb_red);
-        if dist < 1.55
+        dist = CalcDist_Red(frame_w,img_ave_black,eigb_black);
+        if dist < 2
             rectangle('Position',rec+[-5,-5 10 10]','EdgeColor','cyan','LineWidth',2);
+        else
+            dist
         end
+        
     end
 else
     coord = [];
     return
- 
+    
 end
 
 coord = [1,1];
+
 
 end
--- runvideo.m
+++ runvideo.m
@@ -18,7 +18,7 @@ end
 % %loop = [15 20]; % pigs 1
 % loop = [25 28]; % pigs 2 
 %loop = [31 36]; % pigs 3
-loop = [1,40];
+loop = [54,60];
 
 video.CurrentTime = loop(1);
 toc_0 = 0;
@@ -26,10 +26,12 @@ tic
 frame_count = 1;
 
 [eigb_red,img_ave_red] = find_eigbird_red(15);
+[eigb_black,img_ave_black] = find_eigbird_black(15);
 
 
-win_size = [19,21];
 
+
+% pause(3)
 
 
 while hasFrame(video)
@@ -49,7 +51,6 @@ while hasFrame(video)
     frame_med(:,:,1) = medfilt2(frame(:,:,1),[3,3]);
     frame_med(:,:,2) = medfilt2(frame(:,:,2),[3,3]);
     frame_med(:,:,3) = medfilt2(frame(:,:,3),[3,3]);
-
     
     
     frame_red_bird = CropColour(frame_med,[140,220,0,30,30,70]);
@@ -63,73 +64,17 @@ while hasFrame(video)
     
 
     if sum(sum(frame_black_bird)) >= 5
-        [frame_black_bird,region_k] = Filter_Black(frame_black_bird);
+        [frame_black_bird,rec_k] = Filter_Black(frame_black_bird);
+        coord_k = confirm_black_bird(frame,img_ave_black,eigb_black,rec_k);
     end
     
     if sum(sum(frame_pig)) >= 5
         [frame_pig,region_p] = Filter_Pig(frame_pig);
-    end
-    
-    
-    
-    
-    
-    
+    end  
     
 %     subplot(1,2,2)
 %     image(frame_pig*1000)
 %     %
-%     [centres,radii] = imfindcircles(frame,[10,14],'Sensitivity',0.95,'EdgeThreshold',0.3...
-%         ,'ObjectPolarity','dark');
-%     viscircles(centres, radii,'EdgeColor','b');
-%         [centres,radii] = imfindcircles(frame,[10,14],'Sensitivity',0.95,'EdgeThreshold',0.3...
-%         ,'ObjectPolarity','dark');
-
-    
-% %     
-    
-    
-    
-    
-    %% PCA part
-    % for i = 1:size(region)
-    %     rec = region(i).BoundingBox;
-    %     bird_found = 0;
-    %     for w = win_size
-    %
-    %         x_list =round(rec(1)+floor(w/2):5:rec(1)+rec(3)-floor(w/2));
-    %         y_list =round(rec(2)+floor(w/2):5:rec(2)+rec(4)-floor(w/2));
-    %
-    %         for y = y_list
-    %             for x = x_list
-    %
-    % %                 x
-    % %                 y
-    %                 frame_w = frame(y-floor(w/2):y+floor(w/2),...
-    %                     x-floor(w/2):x+floor(w/2),:);
-    % %                 subplot(1,2,2)
-    % %                 image(frame_w)
-    % %                 pause(0.5)
-    %                 dist = CalcDist_Red(frame_w,img_ave_red,eigb_red);
-    %
-    %                 if dist <= 1.4
-    %                     bird_found = 1;
-    %                     rec_bird = rectangle('Position',[x-floor(w/2),y-floor(w/2),w,w]);
-    %                     break
-    %                 end
-    %
-    %             end
-    %             if bird_found
-    %                 break
-    %             end
-    %         end
-    %         if bird_found
-    %             break
-    %         end
-    %
-    %     end
-    % end
-    %%
     
     if frame_count == 10
         clc
