36da9dd0470cac83742fe36570f0457c7fc3b589
Tony <u5889098@anu.edu.au>
2017-05-21 16:54:00 +1000

    trajectory_sketch

---
+++ trajectory_sketch.m
@@ -0,0 +1,26 @@
+function [coord_store,para_traj] = trajectory_sketch(frame_count,birds)
+
+if mod(frame_count,6) == 0
+    coord_store = NaN(2,3);
+    coord_store(1,1) = birds(x);
+    coord_store(1,2) = birds(y);
+else 
+    if mod(frame_count,6) == 1
+        coord_store(2,1) = birds(x);
+        coord_store(2,2) = birds(y);
+    else 
+        if mod(frame_count,6) == 2
+            coord_store(3,1) = birds(x);
+            coord_store(3,2) = birds(y);
+        end
+    end
+end
+
+if sum(isnan(coord_store)) == 0
+    x = coord_store(1,:);
+    y = coord_store(2,:);
+    para_traj = polyfit(x,y,2);
+end
+end
+    
+    
