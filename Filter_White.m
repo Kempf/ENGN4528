fcc47f43e3e8f006cb9260b8dfc51ac91fc32538
JaeSim0102 <wodnjs9966@gmail.com>
2017-05-25 02:14:01 +1000

    Update Filter_White.m

--- Filter_White.m
+++ Filter_White.m
@@ -25,7 +25,7 @@ colour_detected_rec = [];
             
             region(i).BoundingBox = region(i).BoundingBox + [-3,-13,5,15];
 			
-			coord = [coord; region(i).BoundingBox(1)+region(i).BoundingBox(3), region(i).BoundingBox(2)+region(i).BoundingBox(4)];
+			coord = [coord; region(i).BoundingBox(1)+region(i).BoundingBox(3), region(i).BoundingBox(2)+region(i).BoundingBox(4),0,4];
             
             window = CropColour(imcrop(frame_ad,region(i).BoundingBox),[255,255,200,250,100,190]);
             if sum(window(:)) >= 10
