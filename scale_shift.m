c2e792db34e71575529c590b36bbd5bf4c3b35b0
Yitong-Chen <cyt20141646@gmail.com>
2017-05-24 19:52:36 +1000

    output tranformation matrix from pre. to current

--- scale_shift.m
+++ scale_shift.m
@@ -1,7 +1,7 @@
-function [ scale_ratio,x_shift,y_shift ] = scale_shift( frame1,frame2 )
+function [ scale_ratio,x_shift,y_shift,tform ] = scale_shift( frame1,frame2 )
 
-original = rgb2gray(frame1(60:320,:,:));
-distorted = rgb2gray(frame2(60:320,:,:));
+original = rgb2gray(frame1(60:end,:,:));
+distorted = rgb2gray(frame2(60:end,:,:));
 
 ptsOriginal  = detectSURFFeatures(original);
 ptsDistorted = detectSURFFeatures(distorted);
@@ -22,7 +22,6 @@ ss = Tinv(2,1);
 sc = Tinv(1,1);
 transform=Tinv(3,1:2)
 scale_recovered = sqrt(ss*ss + sc*sc)
-theta_recovered = atan2(ss,sc)*180/pi;
 
 scale_ratio=scale_recovered;
 x_shift=transform(1);
