29589698158975ec53bfcb55aef0557f153a2d47
JaeSim0102 <wodnjs9966@gmail.com>
2017-05-25 01:15:38 +1000

    Update runvideo.m

--- runvideo.m
+++ runvideo.m
@@ -26,12 +26,14 @@ en_white = 1;
 en_pig = 1;
 en_sling = 1;
 
+
+
 % video start and end
-loop = [12 60]; % whole video
+% loop = [12 60]; % whole video
 % loop = [14 20]; % red birds
 % loop = [22 28]; % blue birds
-% loop = [28 31]; % yellow birds
-% loop = [37 40]; % black birds
+% loop = [29 32]; % yellow birds
+loop = [37 40]; % black birds
 % loop = [45 48]; % white bird
 % loop = [15 20]; % pigs 1
 % loop = [25 28]; % pigs 2
@@ -81,7 +83,7 @@ bird_pixel_prev = [];
 
 whiteout_time = 0;
 background_displacement = [0,0];
-scale_recorded = 0;
+sling_recorded = 0;
 scale = 1;
 while hasFrame(video)
     
@@ -139,11 +141,15 @@ while hasFrame(video)
     % detect slingshot
     if en_sling
         [sling_coord,rec_drawn_s] = Filter_Slingshot(frame);
+    else
+        sling_coord = [];
     end
     
     %     % detect red birds
     if  en_red
         [red_coord,rec_r,rec_drawn_r] = Filter_Red(frame);
+    else
+        red_coord = [];
     end
     
     %     % detect blue birds
@@ -154,78 +160,79 @@ while hasFrame(video)
     % detect yellow bird
     if en_yellow
         [yellow_coord,rec_y,rec_drawn_y] = Filter_Yellow(frame);
+    else
+        yellow_coord = [];
     end
     
     %     % black birds
     if en_black
         [black_coord,rec_k,rec_drawn_k] = Filter_Black(frame);
+    else
+        black_coord = [];
     end
     
     % detect white bird
     if en_white
         [white_coord,rec_w,rec_drawn_w] = Filter_White(frame);
+    else
+        white_coord = [];
     end
     
     %    % detect pigs
     if en_pig
         [pig_coord,rec_g,rec_drawn_g] = Filter_Pig(frame);
+    else
+        pig_coord = [];
     end
     
     bird_pixel =[red_coord;blue_coord;black_coord;yellow_coord;white_coord];
     
+    
+    
+    
+    
     if isequal(frame,255*ones(320,480,3))
         whiteout_time = video.CurrentTime;
         en_sift = 0;
+        sling_recorded = 0;
+        scale = 1;
+        background_displacement  = [0,0];
     end
     
     if video.CurrentTime > whiteout_time + 1
         en_sift = 1;
         whiteout_time = 0;
     end
-
-    %0:Undertermined
-    %1:Birds on the ground
-    %2:Bird on the slingshot
-    %3:Bird Launched
+    
     if en_sift == 1;
         [ scale_ratio,x_shift,y_shift,~ ] = scale_shift(frame_prev,frame);
         x_shift = round(x_shift);
         y_shift = round(y_shift);
-        if scale_recorded == 1;
+        if sling_recorded == 1;
             scale = scale*(1/scale_ratio);
             x_shift_scaled = round(x_shift/scale);
             y_shift_scaled = round(y_shift/scale);
         end
         
     end
-    bird_coord = bird_pixel;
-    for i = 1:size(bird_pixel,1)
-        if scale_recorded == 1;
+    
+    
+    if (sling_recorded == 0) &&  x_shift == 0 && y_shift == 0;
+        gen_sling_coord = sling_coord;
+        sling_recorded = 1;
+        
+    elseif sling_recorded == 1;
+        if sling_recorded == 1;
+            
             background_displacement = background_displacement + [x_shift_scaled,y_shift_scaled];
         end
-        
-        if ~isempty(sling_coord)
-            bird_coord(i,1) =  round(scale*(bird_pixel(i,1) - sling_coord(1)));
-            bird_coord(i,2) = round(scale*(sling_coord(2) - bird_pixel(i,2)));
+        bird_coord = bird_pixel;
+        for i = 1:size(bird_pixel,1)
+            bird_coord(i,1) = scale*bird_pixel(i,1)- background_displacement(1)-gen_sling_coord(1);
+            bird_coord(i,2) = gen_sling_coord(2) - background_displacement(2)-scale*bird_pixel(i,2);
             bird_coord(i,3) = 1;
         end
-        
-        if isempty(sling_coord) %%
-            
-        
-        end
-        
-        if bird_coord(i,3) == 1  && bird_coord(i,2) >30
-            bird_coord(i,3) = 2;
-        end
-        
-        if bird_coord(i,3) == 2  || bird_coord(i,3) == 0
-            if bird_coord(i,1) > 3
-                bird_coord(i,3) = 3;
-            
-            end
-        end
-        
+        prev_bird_coord =  bird_coord
         
         
     end
@@ -233,23 +240,21 @@ while hasFrame(video)
     
     
     
-    prev_bird_coord =  bird_coord
-        
-
+    
     
     drawnow
     delete(frame_obj)
     % fps counter
-    if frame_count == 10
-        frame_count = 1;
-        toc_1 = toc;
-        time_per_frame = (toc_1 - toc_0)/10;
-        frame_per_sec = 1/time_per_frame;
-        msg = sprintf('FPS: %.1f\n', frame_per_sec);
-        fprintf([clc_str, msg]);
-        clc_str = repmat(sprintf('\b'), 1, length(msg));
-        toc_0 = toc_1;
-    end
+    %     if frame_count == 10
+    %         frame_count = 1;
+    %         toc_1 = toc;
+    %         time_per_frame = (toc_1 - toc_0)/10;
+    %         frame_per_sec = 1/time_per_frame;
+    %         msg = sprintf('FPS: %.1f\n', frame_per_sec);
+    %         fprintf([clc_str, msg]);
+    %         clc_str = repmat(sprintf('\b'), 1, length(msg));
+    %         toc_0 = toc_1;
+    %     end
     frame_count = frame_count + 1;
     
     frame_prev = frame;
